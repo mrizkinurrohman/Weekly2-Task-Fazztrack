61e6de530b5d3a1ae93f6f6b1c1a6bab6e93293e
code with rizki <mrizki.nurrohman10@gmail.com>
2024-12-01 15:18:07 +0700

    Memasukan file enam.js

---
+++ NOMOR6/enam.js
@@ -0,0 +1,25 @@
+function divideAndSort(number) {
+    // Konversi angka menjadi string dan pisahkan berdasarkan '0'
+    const parts = number.toString().split("0");
+    // console.log(parts);
+  
+    // Sorting setiap bagian dan gabungkan kembali
+    const sortedParts = parts.map((part) => {
+      return part.split("").sort().join("");
+    });
+  
+    // Gabungkan semua bagian yang sudah disortir
+    const result = sortedParts.join("");
+    console.log(result);
+  
+    // Konversi kembali ke angka jika diperlukan (optional)
+    return parseInt(result, 10);
+  }
+  
+  // Contoh penggunaan
+  const inputNumber = 5956560159466056;
+  const output = divideAndSort(inputNumber);
+  console.log("Input:", inputNumber);
+  console.log("Output:", output);
+
+  
